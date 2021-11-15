d5401e0324154a6c43f1f2d5f1598a1c8b46d291
abenakesseh <abenakesseh@yahoo.com>
2021-11-15 17:13:45 -0600

    changes made in app.js

--- movie-finder/js/app.js
+++ movie-finder/js/app.js
@@ -7,7 +7,7 @@ $(document).ready(() => {
 });
 
 function getMovies(searchText){
-	axios.get('http://www.omdbapi.com?&apikey=193f73c7&s='+searchText)
+	axios.get('https://www.omdbapi.com?&apikey=193f73c7&s='+searchText)
 	.then((response) => {
 		console.log(response);
 		let movies = response.data.Search;
@@ -40,7 +40,7 @@ function movieSelected(id) {
 function getMovie(){
 	let movieId = sessionStorage.getItem('movieId');
 
-	axios.get('http://www.omdbapi.com?&apikey=193f73c7&i='+movieId)
+	axios.get('https://www.omdbapi.com?&apikey=193f73c7&i='+movieId)
 	.then((response) => {
 		console.log(response);
 		let movie = response.data;
@@ -69,7 +69,7 @@ function getMovie(){
 					<h3> Plot</h3>
 					${movie.Plot}
 					<hr>
-					<a href="http://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
+					<a href="https://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
 					<a href="index.html" class="btn btn-default">Go Back To Search</a>
 				</div>
 			</div>
